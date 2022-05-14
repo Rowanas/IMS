16aadc868f5f13d650b9f6012d9d489664893ae7
Rowanas <103577456+Rowanas@users.noreply.github.com>
2022-05-14 09:43:50 +0100

    Delete target directory
    Target was in gitignore, not sure why it's here.

--- target/classes/sql-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `ims`.`item` (`item_name`, `item_price`) VALUES ('pliers', 3.99); 
-INSERT INTO `ims`.`orders` (`customer_id`) VALUES (1); 
--- target/test-classes/sql-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `customers` (`forename`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `item` (item_name, item_price) VALUES ('pliers', 3.99);
-INSERT INTO `orders` (customer_id) VALUES (1);
-INSERT INTO `order_items` (order_id, item_id) VALUES (1,1);
